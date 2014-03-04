2bdab56f52caf05b34a247a53fe5adeb706e53ca
picasso250 <cumt.xiaochi@gmail.com>
2014-03-04 23:55:01 +0800

    transfer command

--- client/main.py
+++ client/main.py
@@ -1,12 +1,41 @@
 import socket
 import sys
+import json
+
+# cmd table
+# u upload --rs-- file name --rs-- file content
+# d
+
+def upload_file(filename):
+    s = socket.socket()
+    s.connect(("localhost",9999))
+    f=open (filename, "rb") # read binary
+    l = f.read(1024)
+    while (l):
+        s.send(l)
+        l = f.read(1024)
+    s.close()
+
+
 
 s = socket.socket()
 s.connect(("localhost",9999))
-f=open ("test.txt", "rb") # read binary
-l = f.read(1024)
-while (l):
-    s.send(l)
-    l = f.read(1024)
+
+head = json.dumps({
+    'cmd': 'upload',
+    'name': 'test.txt'
+})
+length = len(head)
+print(head)
+print(length)
+
+import struct
+length = struct.pack('i', length)
+print length
+
+
+s.send(length)
+s.send(head)
+s.send('file content\n')
 s.close()
 
--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+
 s = socket.socket()
 s.bind(("localhost",9999))
 s.listen(10) # Acepta hasta 10 conexiones entrantes.
@@ -7,15 +8,38 @@ s.listen(10) # Acepta hasta 10 conexiones entrantes.
 while True:
     sc, address = s.accept()
 
-    print address
-    f = open('file_'+".txt",'wb') #open in binary
-    l = sc.recv(1024)
-    print('.')
-    while (l):
-            f.write(l)
-            l = sc.recv(1024)
-    f.close()
+    length = sc.recv(4)
+    print length
+    import struct
+    length = struct.unpack('i', length)
+    print length
+    head = sc.recv(length[0])
+    print head
+    import json
+    head = json.loads(head)
+    print head
+
+    # l = sc.recv(1024)
+    # while (l):
+    #     print('-\t'+l+'\n--')
+    #     l = sc.recv(1024)
 
     sc.close()
 
 s.close()
+
+# while True:
+#     sc, address = s.accept()
+
+#     print address
+#     f = open('file_'+".txt",'wb') #open in binary
+#     l = sc.recv(1024)
+#     print('.')
+#     while (l):
+#             f.write(l)
+#             l = sc.recv(1024)
+#     f.close()
+
+#     sc.close()
+
+# s.close()
